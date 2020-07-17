8c15d9d484e4e3bcfe8a84c1964396870db7b8ac
Jessica <duellje@oregonstate.edu>
2020-07-17 11:22:06 -0400

    set up axios call

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 //context
-import SmurfContext from '../contexts/SmurfContext'
+import {SmurfContext} from '../contexts/SmurfContext'
 
 //components 
 import SmurfList from './SmurfList';
 
 
 const App = () => {
+  const [smurfList, addSmurf] = useState([]);
+  const [error, setError] = useState("");
+ 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        addSmurf(res.data);
+      })
+      .catch(err => {
+        setError(err.message);
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
       <div>Welcome to your state management version of Smurfs!</div>
-      <SmurfContext.Provider>
+      <SmurfContext.Provider value={{smurfList}}>
         <SmurfList />
       </SmurfContext.Provider>
     </div>
