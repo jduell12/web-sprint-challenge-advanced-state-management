4ad7562b047a1314ac2d89bfcdff26f69a98dd2c
Jessica <duellje@oregonstate.edu>
2020-07-17 11:59:04 -0400

    added form and handlechange function

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,26 +2,59 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 //context
-import {SmurfContext} from '../contexts/SmurfContext'
+import {SmurfContext} from '../contexts/SmurfContext';
+import {FormContext} from '../contexts/FormContext';
 
 //components 
 import SmurfList from './SmurfList';
+import SmurfForm from './SmurfForm';
 
 
 const App = () => {
-  const [smurfList, addSmurf] = useState([]);
-  const [error, setError] = useState("");
- 
-  useEffect(() => {
+
+  const initialFormValues = {
+    name: "",
+    age: 0,
+    height: ""
+    
+  }
+
+  const [formValues, setValues] = useState(initialFormValues);
+  // const [formErrors, setErrors] = useState("");
+
+  const handleChange = e => {
+    setValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
     axios
-      .get('http://localhost:3333/smurfs')
+      .post(`http://localhost:3333/smurfs/${formValues}`)
       .then(res => {
-        addSmurf(res.data);
+        console.log(res);
       })
       .catch(err => {
-        setError(err.message);
+        console.log(err);
       })
+  }
+
+  const [smurfList, addSmurf] = useState([]);
+  
+  useEffect(() => { 
+    axios
+          .get('http://localhost:3333/smurfs')
+          .then(res => {
+            addSmurf(res.data);
+          })
+          .catch(err => {
+            console.log(err.message);
+          })
+
   }, [])
+
 
   return (
     <div className="App">
@@ -29,6 +62,9 @@ const App = () => {
       <div>Welcome to your state management version of Smurfs!</div>
       <SmurfContext.Provider value={{smurfList}}>
         <SmurfList />
+        <FormContext.Provider value={{formValues, handleChange, onSubmit}}>
+          <SmurfForm />
+        </FormContext.Provider>
       </SmurfContext.Provider>
     </div>
   );
---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+
+//context 
+import {FormContext} from '../contexts/FormContext'
+
+const SmurfForm = () => {
+
+    const {formValues, handleChange, onSubmit} = useContext(FormContext);
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name"> 
+                Name: &nbsp;
+                <input id="name" name="name" type="text" placeholder="Enter the smurf name..." value={formValues.name} onChange={handleChange}/>
+            </label>
+            <br />
+            <label htmlFor="age">
+                Age: &nbsp;
+                <input id="age" name="age" type="number" step="1" min="0" placeholder="Enter the smurf age..." value={formValues.age} onChange={handleChange}/>
+            </label>
+            <br />
+            <label htmlFor="height">
+                Height: &nbsp;
+                <input id="height" name="height" type="text" placeholder="Enter in the smurf height" value={formValues.height} onChange={handleChange} />
+            </label>
+            <br />
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default SmurfForm;
