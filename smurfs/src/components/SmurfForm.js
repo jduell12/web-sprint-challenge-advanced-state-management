14089b093685943d17399769312cc55fb1dda514
Jessica <duellje@oregonstate.edu>
2020-07-17 13:48:07 -0400

    cleaned up code

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -13,24 +13,6 @@ const SmurfForm = () => {
     return (
         <>
             <h2>Add A Smurf To The Village...</h2>
-            {/* <form onSubmit={onSubmit}>
-            <label htmlFor="name"> 
-                    Name: &nbsp;
-                    <input id="name" name="name" type="text" placeholder="Enter the smurf name..." value={formValues.name} onChange={handleChange}/>
-                </label>
-                <br />
-                <label htmlFor="age">
-                    Age: &nbsp;
-                    <input id="age" name="age" type="number" step="1" min="0" placeholder="Enter the smurf age..." value={formValues.age} onChange={handleChange}/>
-                </label>
-                <br />
-                <label htmlFor="height">
-                    Height: &nbsp;
-                    <input id="height" name="height" type="text" placeholder="Enter in the smurf height" value={formValues.height} onChange={handleChange} />
-                </label>
-                <br />
-                <button>Submit</button>
-            </form> */}
                 <StyledForm onSubmit={onSubmit}>
                 <label htmlFor="name"> 
                     Name: &nbsp;
@@ -49,6 +31,24 @@ const SmurfForm = () => {
                 <br />
                 <button>Submit</button>
             </StyledForm>
+                      {/* <form onSubmit={onSubmit}>
+            <label htmlFor="name"> 
+                    Name: &nbsp;
+                    <input id="name" name="name" type="text" placeholder="Enter the smurf name..." value={formValues.name} onChange={handleChange}/>
+                </label>
+                <br />
+                <label htmlFor="age">
+                    Age: &nbsp;
+                    <input id="age" name="age" type="number" step="1" min="0" placeholder="Enter the smurf age..." value={formValues.age} onChange={handleChange}/>
+                </label>
+                <br />
+                <label htmlFor="height">
+                    Height: &nbsp;
+                    <input id="height" name="height" type="text" placeholder="Enter in the smurf height" value={formValues.height} onChange={handleChange} />
+                </label>
+                <br />
+                <button>Submit</button>
+            </form> */}
         </>
     )
 }
